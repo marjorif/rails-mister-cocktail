9e3a16c123f111aca40858f5d5a0ade017db04cb
Marjori Fonte <marjorif@live.com>
2020-10-10 14:49:15 +0100

    Validations done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 
 end
