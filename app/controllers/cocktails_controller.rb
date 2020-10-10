fe93ea28b1f486dbcc4950f9adc50c9d79788a37
Marjori Fonte <marjorif@live.com>
2020-10-10 16:55:06 +0100

    Fixing ends

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -17,11 +17,12 @@ class CocktailsController < ApplicationController
       redirect_to cocktail_path(@cocktail)
     else
       render "new"
+    end
   end
-end
 
-private
+  private
 
-def restaurant_params
-  params.require(:cocktail).permit(:name)
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
